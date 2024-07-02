2309a021ef913a1d245f9120c1f02f263a778f7a
mani chandana <nmcreddy2000@gmail.com>
2024-07-02 17:06:37 +0530

    Minor changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -12,16 +13,19 @@ class Header extends Component {
                     <li><a href='/auth/google'>Login With Google</a></li>
                 )
             default:
-                return <li><a>Logout</a></li>
+                return <li><a href='/api/logout'>Logout</a></li>
         }
     }
     render() {
         return (
             <nav>
                 <div className='nav-wrapper'>
-                    <a className='left brand-logo'>
+                    <Link 
+                    className='left brand-logo' 
+                    to={this.props.user ? "/surveys" : "/"}
+                    >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className='right'>
                         {this.renderContent()}
                     </ul>
